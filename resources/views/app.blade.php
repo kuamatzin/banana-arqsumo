532d1b00d25aae66c0bad117166def1efde28331
Carlos Cuamatzin <kuamatzin@gmail.com>
2018-09-19 14:12:23 -0500

    Video background

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,13 +8,13 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>ARQMASUNO</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app" class="mb-5">
+    <div id="app">
         @yield('content')
     </div>
 
